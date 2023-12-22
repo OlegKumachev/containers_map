b62f106d7aa0900c1ca493fc4dc1a6e74cd90ba5
OlegKumachev <117755901+OlegKumachev@users.noreply.github.com>
2023-12-22 19:02:09 +0300

    Update test.js

--- src/__test__/test.js
+++ src/__test__/test.js
@@ -1,14 +1,14 @@
-// import ErrorRepository from "../app";
+import ErrorRepository from "../app";
 
-// test('test 1', () => {
-//     const val = new ErrorRepository();
-//     val.addError(1, 'Ошибка выбора');
-//     expect(val.translate(1)).toBe('Ошибка выбора');
-// });
+test('test 1', () => {
+    const val = new ErrorRepository();
+    val.addError(1, 'Ошибка выбора');
+    expect(val.translate(1)).toBe('Ошибка выбора');
+});
 
-// test('test 2', () => {
-//     const val = new ErrorRepository();
-//     expect(val.translate(2)).toBe('Unknown error');
+test('test 2', () => {
+    const val = new ErrorRepository();
+    expect(val.translate(2)).toBe('Unknown error');
 
-// });
+});
 
